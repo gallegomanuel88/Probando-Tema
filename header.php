ff65470740a5d03797dec5432efb1ced741d3c4e
Manu <malonsorodriguez@danielcastelao.org>
2016-12-11 15:46:27 +0000

    Header añadido

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="styleSheet"  type="text/css" href="<?php echo get_stylesheet_uri();?>">
+        
+    </head>
+    
+    <body>
+ +       <?php wp_nav_menu(array('Menu uno' => 'Menu dos'));?>
+        <a href="https://wordpressmanu-gallegomanuel88.c9users.io/">
+            
+            <div id="header" />
+        </a>
