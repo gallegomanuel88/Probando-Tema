160f167c81cb74abf9577cf41a0beb87936be4a4
Manu <malonsorodriguez@danielcastelao.org>
2016-12-11 15:52:44 +0000

    404 añadido

---
+++ 404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>" type="text/css" />
+    </head>
+    <body>
+        <div id="error404">
+            <h1>ERRORSITO 404</h1>
+            <img src="<?php echo get_template_directory_uri()?>/images/imagen404.gif"/>
+        </div>
+    </body>
+</html>
